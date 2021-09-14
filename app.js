7691b789f38052c86dff21aeaf495c5121ee5566
Silvia Castro <silviaj.castrofernandez@gmail.com>
2021-09-13 22:26:39 -0600

    Second attempt no inline JS in HTML

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-let number = document.querySelector(".like");
-let likes = 4;
+// let number = document.querySelector(".like");
+// let likes = 4;
+
+// function addLike() {
+//     number.innerText = ` ${likes} likes`
+//     likes ++;
+// }
+
+let btn = document.getElementById("btn")
+let allDiv = btn.closest("div");
+let likes = allDiv.querySelector(".like");
+let numlikes = 4;
 
 function addLike() {
-    number.innerText = ` ${likes} likes`
-    likes ++
+    likes.innerText =  ` ${numlikes} likes`;
+    numlikes ++;
 }
+
+
+
+btn.addEventListener("click", addLike)
 
 
